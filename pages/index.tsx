304c7eeb90873ede26b9f5583323c867262e1f61
Greg Tambornino <greg.tambornino@concordusa.com>
2023-08-11 13:31:39 -0500

    convert some files to ts, adding next types

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import Head from "next/head";
 import Link from "next/link";
+import { GetStaticProps } from "next";
 import Date from "../components/date";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = () => {
   const allPostsData = getSortedPostsData();
   return {
     props: {
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,39 +1,41 @@
 import Layout from "../../components/layout";
 import Head from "next/head";
+import Home from "../index";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
+import { GetStaticPaths, GetStaticProps } from "next";
 
-export async function getStaticProps({ params }) {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const postData = await getPostData(params.id);
   return {
     props: {
       postData,
     },
   };
-}
+};
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds();
   return {
     paths,
     fallback: false,
   };
-}
+};
 
 export default function Post({ postData }) {
   return (
-    <Layout>
+    <Layout home={false}>
       <Head>
         <title>{postData.title}</title>
       </Head>
       <article>
         <h1 className={utilStyles.headingXL}>{postData.title}</h1>
-        
+
         <div className={utilStyles.lightText}>
-        <Date dateString={postData.date} />
+          <Date dateString={postData.date} />
         </div>
-        
+
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
     </Layout>
