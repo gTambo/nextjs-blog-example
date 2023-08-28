36588dd847abd029d0f7879a23f479d3327ed04a
Greg Tambornino <greg.tambornino@concordusa.com>
2023-08-28 17:22:59 -0500

    generic router test

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -1,14 +1,19 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import userEvent from '@testing-library/user-event';
-import { useRouter } from 'next/router'; // Next.js's useRouter for routing
-// import mockRouter from 'next-router-mock';
+// import { useRouter } from 'next/router';
+import mockRouter from 'next-router-mock';
+import { MemoryRouterProvider } from 'next-router-mock/MemoryRouterProvider';
+import { createDynamicRouteParser } from "next-router-mock/dynamic-routes";
 import Home from "../pages/index";
 import "@testing-library/jest-dom";
 import { getSortedPostsData } from "../lib/posts";
 
-jest.mock('next/router', () => ({
-  useRouter: jest.fn(),
-}));
+// jest.mock('next/router', () => jest.requireActual('next-router-mock'));
+mockRouter.useParser(createDynamicRouteParser(['/', '/posts/[id]']));
+// jest.mock('next/router', () => ({
+//   useRouter: jest.fn(), 
+  // usePathname: jest.fn().mockReturnValue("/posts/ssg-ssr"),
+// }));
 
 describe("Home", () => {
   const allPostsData = getSortedPostsData();
@@ -28,5 +33,32 @@ describe("Home", () => {
     const blogPosts = await screen.findAllByRole('listitem');
 
     expect(blogPosts.length).toBeGreaterThanOrEqual(1);
+  });
+  it("navigates to another page when clicking on the blog post", async () => {
+    // const mockedRouter = {
+    //   push: jest.fn(),
+    //   pathname: '/',
+    // };
+    // useRouter.mockReturnValue(mockedRouter);
+    render(<Home allPostsData={allPostsData} />);
+
+    expect(mockRouter).toMatchObject({
+      pathname: '/',
+      query: {}
+    });
+    // let blogLink = await screen.getAllByRole('listitem');
+    // expect(blogLink.length).toBe(2);
+    // userEvent.click(screen.getByText('When to Use Static Generation v.s. Server-side Rendering'));
+    // expect(mockRouter).toHaveBeenCalled();
+    // expect(screen.queryByRole("link", {
+    //   name: /Strava/i,
+    // })).not.toBeInTheDocument();
+    
+    mockRouter.push('/posts/ssg-ssr');
+    expect(mockRouter).toMatchObject({
+      pathname: '/posts/[id]',
+      query: { id: 'ssg-ssr' }
+    });
+
   });
 });
