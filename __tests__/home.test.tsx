40d818fccd94aef2565cf12c046ab7bc84b9e93c
Greg Tambornino <greg.tambornino@concordusa.com>
2023-08-28 15:44:35 -0500

    remove nav test

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -1,17 +1,32 @@
-import { render, screen } from '@testing-library/react'
-import Home from '../pages/index'
-import '@testing-library/jest-dom'
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
+import { useRouter } from 'next/router'; // Next.js's useRouter for routing
+// import mockRouter from 'next-router-mock';
+import Home from "../pages/index";
+import "@testing-library/jest-dom";
 import { getSortedPostsData } from "../lib/posts";
- 
-describe('Home', () => {
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(),
+}));
+
+describe("Home", () => {
   const allPostsData = getSortedPostsData();
-  it('renders a heading', () => {
-    render(<Home allPostsData={allPostsData}/>) // Arrange
- 
-    const heading = screen.getByRole('heading', {
+  it("renders a heading", () => {
+    render(<Home allPostsData={allPostsData} />); // Arrange
+
+    // screen.debug();
+    const heading = screen.getByRole("heading", {
       name: /Greg T/i,
-    }) // Act
- 
-    expect(heading).toBeInTheDocument()  // Assertion
-  })
-})
+    });
+
+    expect(heading).toBeInTheDocument(); // Assertion
+  });
+  it("renders a list of one or more blog posts", async () => {
+    render(<Home allPostsData={allPostsData} />);
+
+    const blogPosts = await screen.findAllByRole('listitem');
+
+    expect(blogPosts.length).toBeGreaterThanOrEqual(1);
+  });
+});
